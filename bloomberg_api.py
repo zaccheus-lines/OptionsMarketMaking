b2216c324d74a0bfe05faafe8befd2694e0cc857
zaccheus-lines <zaccheus.lines@hotmail.com>
2023-12-01 18:08:46 +0000

    Retrieve option data

--- bloomberg_api.py
+++ bloomberg_api.py
@@ -1,4 +1,5 @@
 import blpapi
+import time
 
 def main():
     # Create and start a session
@@ -16,14 +17,13 @@ def main():
 
     # Replace these with actual option identifiers
     securities = [
-        "AAPL 12/15/23 C185 Equity", "AAPL 12/15/23 C185 Equity"
+        "AAPL 12/15/23 C185 Equity"
     ]
 
 
     fields = [
-        "OPT_STRIKE_PX", "DAYS_TO_EXPIRY",
-        "BID", "BID_SIZE", "ASK", "ASK_SIZE",
-        "BID_PX", "BID_SIZE", "ASK_PX", "ASK_SIZE"
+        "OPT_STRIKE_PX", "OPT_EXPIRE_DT",
+        "BID", "BID_SIZE", "ASK", "ASK_SIZE"
     ]
 
     for security in securities:
@@ -36,19 +36,25 @@ def main():
     print("Sending Request:", request)
     session.sendRequest(request)
 
+
     # Process received events
+    
     while True:
-        ev = session.nextEvent()
+        ev = session.nextEvent(500)
         for msg in ev:
             if ev.eventType() == blpapi.Event.RESPONSE:
                 print("Response received:", msg)
                 # Process the response message to extract data
+                
 
         if ev.eventType() == blpapi.Event.RESPONSE:
             # End of the response
             break
-
+        
     session.stop()
+    securityData = msg['securityData'][0]['fieldData']
+    return securityData
+
 
 if __name__ == "__main__":
-    main()
+    print(main())
